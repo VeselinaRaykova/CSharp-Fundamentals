60711747261f407d48c493c36ddd69ff341100af
VeselinaRaykova <veselinaraikova@gmail.com>
2019-10-14 17:48:24 +0300

    [c#-funds] Add Data types and variables MoreExercise solutions

---
+++ 02DataTypesAndVariables/03DataTypesAndVariables-MoreExercise/06BalancedBrackets/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _06BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char ch;
+            int openingBrackets = 0;
+            int closingBrackets = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (char.TryParse(input, out ch))
+                {
+                    if (ch == '(')
+                    {
+                        openingBrackets++;
+                    }
+                    else if (ch == ')')
+                    {
+                        closingBrackets++;
+                        if (closingBrackets > openingBrackets)
+                        {
+                            Console.WriteLine("UNBALANCED");
+                            return;
+                        }
+                    }
+                }
+            }
+
+            if (openingBrackets == closingBrackets)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
