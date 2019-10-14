66d494559647274cae94446c3c3097ae009ced61
VeselinaRaykova <veselinaraikova@gmail.com>
2019-10-14 11:30:31 +0300

    [c#-funds] Add Data types and variables Exercise solutions

---
+++ 02DataTypesAndVariables/02DataTypesAndVariables-Exercise/07WaterOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int capacity = 255;
+            int liters;
+            int litertsInTank = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                liters = int.Parse(Console.ReadLine());
+                if (liters > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+
+                capacity -= liters;
+                litertsInTank += liters;
+            }
+
+            Console.WriteLine(litertsInTank);
+        }
+    }
+}
---
+++ 02DataTypesAndVariables/02DataTypesAndVariables-Exercise/08BeerKegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxVolume = int.MinValue;
+            string maxKegModel = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                string currKegModel = Console.ReadLine();
+                double r = double.Parse(Console.ReadLine());
+                int h = int.Parse(Console.ReadLine());
+                double currVolume = Math.PI * r * r * h;
+                if (currVolume > maxVolume)
+                {
+                    maxVolume = currVolume;
+                    maxKegModel = currKegModel;
+                }
+            }
+
+            Console.WriteLine(maxKegModel);
+        }
+    }
+}
