88a683166b9dfea56fa7a6d6f187bb6e092e4d35
VeselinaRaykova <veselinaraikova@gmail.com>
2019-12-04 22:08:44 +0200

    Add Associative Arrays Lab solutions

---
+++ 08AssociativeArrays/01AssociativeArrays-Lab/01CountRealNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> uniqueNumbers = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (uniqueNumbers.ContainsKey(numbers[i]))
+                {
+                    uniqueNumbers[numbers[i]] += 1;
+                }
+                else
+                {
+                    uniqueNumbers.Add(numbers[i], 1);
+                }
+            }
+
+            foreach (var pair in uniqueNumbers.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
---
+++ 08AssociativeArrays/01AssociativeArrays-Lab/04Largest3Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _04Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] sorted = numbers.OrderByDescending(x => x).Take(3).ToArray();
+
+            Console.WriteLine(string.Join(" ", sorted));
+        }
+    }
+}
