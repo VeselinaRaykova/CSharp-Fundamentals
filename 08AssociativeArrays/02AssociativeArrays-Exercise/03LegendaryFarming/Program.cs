90acc76337cff4385823612f16dd07a8b53a97e9
VeselinaRaykova <veselinaraikova@gmail.com>
2019-12-05 21:13:32 +0200

    Add Associative arrays exercise solutions

---
+++ 08AssociativeArrays/02AssociativeArrays-Exercise/03LegendaryFarming/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03LegendaryFarming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> materials = new Dictionary<string, int>() {
+            {"shards", 0 }, {"fragments", 0}, {"motes", 0}
+        };
+            Dictionary<string, string> items = new Dictionary<string, string>() {
+            { "motes", "Dragonwrath"}, { "shards", "Shadowmourne"}, { "fragments", "Valanyr"}
+        };
+
+            Dictionary<string, int> junkMaterials = new Dictionary<string, int>() { };
+
+            while (true)
+            {
+                string[] data = Console.ReadLine().Split().ToArray();
+
+                for (int i = 0; i < data.Length; i += 2)
+                {
+                    int quantity = int.Parse(data[i]);
+                    string material = data[i + 1].ToLower();
+
+                    if (materials.ContainsKey(material))
+                    {
+                        materials[material] += quantity;
+
+                        if (materials[material] >= 250)
+                        {
+                            materials[material] -= 250;
+
+                            Console.WriteLine($"{items[material]} obtained!");
+
+                            foreach (var p in materials.OrderByDescending(p => p.Value).ThenBy(p => p.Key))
+                            {
+                                Console.WriteLine($"{p.Key}: {p.Value}");
+                            }
+
+                            foreach (var p in junkMaterials.OrderBy(p => p.Key))
+                            {
+                                Console.WriteLine($"{p.Key}: {p.Value}");
+                            }
+
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        if (!junkMaterials.ContainsKey(material))
+                        {
+                            junkMaterials.Add(material, 0);
+                        }
+
+                        junkMaterials[material] += quantity;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 08AssociativeArrays/02AssociativeArrays-Exercise/04Orders/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Product> orders = new Dictionary<string, Product>();
+
+            string[] data = Console.ReadLine().Split().ToArray();
+
+            while (data[0] != "buy")
+            {
+                string name = data[0];
+                double price = double.Parse(data[1]);
+                int quantity = int.Parse(data[2]);
+
+                if (!orders.ContainsKey(name))
+                {
+                    orders.Add(name, new Product(name, price, quantity));
+                }
+                else
+                {
+                    Product p = orders[name];
+                    p.Price = price;
+                    p.Quantity += quantity;
+                }
+
+                data = Console.ReadLine().Split().ToArray();
+            }
+
+            foreach (var p in orders)
+            {
+                Console.WriteLine($"{p.Value.Name} -> {p.Value.GetTotalPrice():f2}");
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string Name;
+        public int Quantity;
+        public double Price;
+
+        public Product(string name, double price, int quantity)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public double GetTotalPrice()
+        {
+            double total = this.Quantity * this.Price * 1.00;
+            return total;
+        }
+    }
+}
